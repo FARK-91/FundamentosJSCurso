b8ca3eda9849ee5d423f1f9b42a7f101385d4c93
Renier <reniferrer91@gmail.com>
2019-06-21 23:26:39 -0400

    [MOD] Subiendo comentarios

--- clase8.js
+++ clase8.js
@@ -18,12 +18,20 @@ function imprimirNombreEnMayusculas(persona) {
 
 imprimirNombreEnMayusculas(sacha)
 imprimirNombreEnMayusculas(dario)
-// imprimirNombreEnMayusculas({ nombre: 'Pepito' })
-// imprimirNombreEnMayusculas({ apellido: 'Gomez' })
 
+// Cuando pasamos un objeto como parametro de una funcion
+// este se pasa como parametro por referencia, lo que quiere
+// decir que si cambiamos el valos de un atributo dentro de la funcion
+// dicho atributo tambien se vera afectado una vez fuera de la funcion.
 function cumpleanos(persona) {
+  // Para devolver un nuevo objeto:
+  //Nota dentro de las llaves se definen las llaves y valores del nuevo objeto.
   return {
     ...persona,
-    edad: persona.edad + 1
+    edad: persona.edad + 1,
+    nuevo_parametro : 'Nuevo valor'
   }
+
+  // Para solo alterar un parametro utilizo la siguiente sentencia sin el return:
+  // persona.edad += 1
 }
