563ed1788074d7cd63d7ae811c208210819cc1af
Renier <reniferrer91@gmail.com>
2019-06-21 22:58:11 -0400

    [MOD] clase7.js: Anadi codigo del #RetoDeLaClase

--- clase7.js
+++ clase7.js
@@ -1,4 +1,7 @@
+// Esto es un objeto en JS
+// Muy parecido a un dic en Python {Key: Value}
 var sacha = {
+  // Esto un atributo del objeto
   nombre: 'Sacha',
   apellido: 'Lifszyc',
   edad: 28
@@ -10,18 +13,31 @@ var dario = {
   edad: 27
 }
 
+var renier = {
+  nombre: 'Renier',
+  apellido: 'Ferrer',
+  edad: 28
+}
+
+// Como acceder a los datos de un objeto
 function imprimirNombreEnMayusculas(persona) {
-  // var nombre = persona.nombre
+  // Se crea una variable y dentro de las llaves se invoca la clave o atributo del objeto
+  // Luego el iigual se invoca al objeto por medio del parametro persona.
   var { nombre } = persona
   console.log(nombre.toUpperCase())
+  var { apellido } = persona
+  console.log(apellido.toUpperCase())
 }
 
 imprimirNombreEnMayusculas(sacha)
 imprimirNombreEnMayusculas(dario)
-// imprimirNombreEnMayusculas({ nombre: 'Pepito' })
-// imprimirNombreEnMayusculas({ apellido: 'Gomez' })
 
-function imprimirNombreYEdad() {
+function imprimirNombreYEdad(datos) {
   // Hola, me llamo Sacha y tengo 28 años
   // Hola, me llamo Darío y tengo 27 años
+  var {nombre,edad} = datos
+  var some_text = 'Hola, me llamo ' + nombre + ', tengo '+ edad + ' años.'
+  console.log(some_text)
 }
+
+imprimirNombreYEdad(renier)
