adb5aac554219d5000d3a2005bfc4ae29aa681cb
Renier <reniferrer91@gmail.com>
2019-06-30 01:01:34 -0400

    Acceso a un API por medio de Jquery

--- clase28.js
+++ clase28.js
@@ -1,3 +1,4 @@
+// RENIER: Definimos una constante para acceder al URL del API
 const API_URL = 'https://swapi.co/api/'
 const PEOPLE_URL = 'people/:id'
 
@@ -6,7 +7,16 @@ const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`
 const opts = { crossDomain: true }
 
 const onPeopleResponse = function (persona) {
+  // RENIER: Para conocer los argumentos de respuesta podemos imprimir los
+  // argumentos de la Funcion
+  console.log(arguments)
   console.log(`Hola yo soy, ${persona.name}`)
+  console.log(`tengo el cabello de color, ${persona.hair_color}`)
 }
 
+// RENIER: Parametros de un $get
+// 1) URL del API ---> https://swapi.co/api/
+// 2) crossDomain: true
+// 3) Funcion de respuesta, el cual devuelve la data obtenida del API,
+//    el status de la respuesta (exito o fallido) y  por ultimo el objeto Jquery.
 $.get(lukeUrl, opts, onPeopleResponse)
